1899e552d60b1186929d68a5e62c32fef92b03b0
Ryan Johnson <rjohnson4444@live.com>
2015-08-05 15:31:07 -0600

    Refractor the calculator.rb file

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,29 +1,104 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# perform the operation on the two numbers
-# output the results
-
-Kernel.puts("Welcome to Calculator")
-
-Kernel.puts("Please enter the first number")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("Please enter the second number")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("What type of opperation would you like to perform on these two numbers? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-	results = number1.to_i() + number2.to_i()
-elsif operator == '2'
-	results = number1.to_i() - number2.to_i()
-elsif operator == '3'
-	results = number1.to_i() * number2.to_i()
-elsif operator == '4'
-	results = number1.to_f / number2.to_f()
-else
-	puts "Please enter a valid number choice for the opperation"
+def prompt(message)
+	Kernel.puts("=> #{message}")
 end
 
-puts "The answer is: #{results}"
+def valid_number?(num)
+	num.to_i != 0
+end
+
+def operation_to_message(op)
+	case op
+	when '1'
+		'Adding'
+	when '2'
+		'Subtracting'
+	when '3'
+		'Multiplying'
+	when '4'
+		'Dividing'
+	end
+end
+
+
+prompt("Welcome to Calculator! Please enter your name:")
+
+name = ''
+loop do
+	name = Kernel.gets().chomp()
+	if name.empty?
+		prompt("Please enter your name")
+	else
+		break
+	end
+end
+
+prompt("Hi #{name}")
+
+loop do # Main loop
+	number1 = ''
+	loop do
+		prompt("Please enter the first number")
+		number1 = Kernel.gets().chomp()
+
+		if valid_number?(number1)
+			break
+		else
+			prompt("Sorry, but this doesn't look like a valid number. Please try again.")
+		end
+	end
+
+	number2 = ''
+
+	loop do
+		prompt("Please enter the second number")
+		number2 = Kernel.gets().chomp()
+
+		if valid_number?(number2)
+			break
+		else
+			prompt("Sorry, but this doesn't look like a valid number. Please try again.")
+		end
+	end
+
+	operator_prompt = <<-MSG
+		What operation would you like to perform?
+		1) add
+		2) subtract
+		3) multiply
+		4) divide
+	MSG
+
+	prompt(operator_prompt)
+	
+	operator = ''
+	loop do
+		operator = Kernel.gets().chomp()
+
+		if %w(1 2 3 4).include?(operator)
+			break
+		else
+			prompt("Sorry, that's not an option. Please choose 1, 2, 3, or 4")
+		end
+	end
+
+	prompt("#{operation_to_message(operator)} the two numbers...")
+
+	results = case operator
+						when '1'
+							number1.to_i() + number2.to_i()
+						when '2'
+							number1.to_i() - number2.to_i()
+						when '3'
+							number1.to_i() * number2.to_i()
+						when '4'
+							number1.to_f / number2.to_f()
+	end
+
+	prompt("The answer is: #{results}")
+
+	prompt("Do you want to use the calculator again? (Y for yes)")
+	answer = Kernel.gets().chomp()
+	break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thanks for using the calculator! Goodbye.")
---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,50 @@
+# Practice with pseudo code
+
+- A method that returns the sum of two integers
+
+- assign ingeter1 to a variable
+- assign integer2 to a variable
+ - Add the two ingeters together
+- return the sum of the two integers
+
+- A method that takes an array of strings, and returns a string that is all those strings concatenated together.
+
+- iterate through the array of strings
+	- connect all of the strings together
+- return a concatenated string
+
+- A method that takes an array of integers, and returns a new array with every other element
+
+- iterate through the array of integers
+	- keep every other array element
+	- remove the element before and after each element
+- return a new array
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
